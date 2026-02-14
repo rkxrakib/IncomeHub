bb1910e54d15f5cb9fd1a9b9945e2bd275978e9b
rkxrakib <rakibhassanfb2@gmail.com>
2026-02-14 19:27:22 +0600

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvtZJhIN850tU7cETuiqRyCyjCBdlFt-Y",
+  authDomain: "fynora-81313.firebaseapp.com",
+  databaseURL: "https://fynora-81313-default-rtdb.firebaseio.com",
+  projectId: "fynora-81313",
+  storageBucket: "fynora-81313.firebasestorage.app",
+  messagingSenderId: "593306264446",
+  appId: "1:593306264446:web:da476d4c77ae4ede6b492f",
+  measurementId: "G-BX0FWR2YMT"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
